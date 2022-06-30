9c53920459f7f680fe0663fdd04008fb2483a469
Rustambek <rustambeksafarovv@gmail.com>
2022-06-30 17:07:05 +0500

    Redone

--- bin/logical_operators20.dart
+++ bin/logical_operators20.dart
@@ -9,5 +9,21 @@ Five number consisting of one and zero is given (the number starts at once).
     Returns:
         bool: answer
 */
-
-void main() {}
+bool func(n){
+    int a = 0;
+    int b = 0;
+    int x = a%10;
+    int y = (a%100)~/10;
+    int z = (a~/100)%10;
+    int n = ((a~/100)~/10)%10;
+    int m = ((a~/100)~/10)~/10;
+    int birlar = x+y+z+n+m;
+    int nollar = 5-birlar;
+    if (x>0&&m==1&&birlar>nollar){
+        return true;
+    }
+    return false;
+}
+void main() {
+    print(func(11001));
+}
