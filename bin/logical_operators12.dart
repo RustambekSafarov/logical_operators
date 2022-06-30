4e892032014a3c7004ca7d9c1cd325d70c2ea08d
Rustambek <rustambeksafarovv@gmail.com>
2022-06-30 16:52:34 +0500

    Redone

--- bin/logical_operators12.dart
+++ bin/logical_operators12.dart
@@ -7,5 +7,12 @@ Given a two-digit integer a,  check the following statement "All digits of the n
     Returns:
         bool: answer
 */
-
-void main() {}
+bool func(a){
+    int x = (a%10);
+    int y = (a~/10);
+    bool c = x==y;
+    return c;
+}
+void main() {
+    print(func(23));
+}
